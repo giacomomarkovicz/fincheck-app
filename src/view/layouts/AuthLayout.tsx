0465d7efbb090495ad6c4d80a26b8927abcc193a
Giácomo <giacomocamposv2@hotmail.com>
2025-03-20 16:07:04 -0300

    prettier config; format

--- src/view/layouts/AuthLayout.tsx
+++ src/view/layouts/AuthLayout.tsx
@@ -1,19 +1,26 @@
+import { Outlet } from 'react-router-dom'
 import illustration from '../../assets/illustration.png'
 import { Logo } from '../components/Logo'
 
 export function AuthLayout() {
   return (
-    <div className='flex w-full h-full'>
-      <div className='w-1/2 h-full'></div>
-      <div className='w-1/2 h-full flex justify-center items-center p-8'>
+    <div className='flex h-full w-full'>
+      <div className='flex h-full w-full flex-col items-center justify-center gap-16 lg:w-1/2'>
+        <Logo className='h-6 text-gray-500' />
+
+        <div className='w-full max-w-[504px] px-8'>
+          <Outlet />
+        </div>
+      </div>
+      <div className='hidden h-full w-1/2 items-center justify-center p-8 lg:flex'>
         <img
           src={illustration}
-          className='object-cover w-full h-full max-w-[656px] max-h-[960px] select-none rounded-[32px]'
+          className='h-full max-h-[960px] w-full max-w-[656px] rounded-[32px] object-cover select-none'
         />
 
-        <div className='max-w-[656px] bottom-8 bg-white p-10 absolute rounded-b-[32px] '>
-          <Logo className='text-teal-900 h-8' />
-          <p className='text-gray-700 font-medium text-xl mt-6'>
+        <div className='absolute bottom-8 max-w-[656px] rounded-b-[32px] bg-white p-10'>
+          <Logo className='h-8 text-teal-900' />
+          <p className='mt-6 text-xl font-medium text-gray-700'>
             Gerencie suas finanças pessoais de uma forma simples com o fincheck,
             e o melhor, totalmente de graça!
           </p>
