37b3d2708d220b1e13178905bc1b6fc6f1b8fbdb
Giácomo <giacomocamposv2@hotmail.com>
2025-03-26 10:18:39 -0300

    fixed button className

--- src/view/components/Button.tsx
+++ src/view/components/Button.tsx
@@ -1,12 +1,16 @@
 import { ComponentProps } from 'react'
+import { cn } from '../../app/utils/cn'
 
 interface ButtonProps extends ComponentProps<'button'> {}
 
-export function Button(props: ButtonProps) {
+export function Button({ className, ...props }: ButtonProps) {
   return (
     <button
       {...props}
-      className='h-12 rounded-2xl bg-teal-900 px-6 font-medium text-white transition-all hover:bg-teal-800 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-400'
+      className={cn(
+        'h-12 rounded-2xl bg-teal-900 px-6 font-medium text-white transition-all hover:bg-teal-800 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-400',
+        className
+      )}
     />
   )
 }
