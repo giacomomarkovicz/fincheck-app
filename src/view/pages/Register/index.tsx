f23c539c6fcf3e4134e38c4e9d9cd883dc2b7970
Giácomo <giacomocamposv2@hotmail.com>
2025-03-26 10:19:23 -0300

    register form

--- src/view/pages/Register/index.tsx
+++ src/view/pages/Register/index.tsx
@@ -1,8 +1,11 @@
 import { Link } from 'react-router-dom'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
+import { useRegisterController } from './useRegisterController'
 
 export function Register() {
+  const { errors, handleSubmit, register } = useRegisterController()
+
   return (
     <div>
       <header className='flex flex-col items-center gap-4 text-center'>
@@ -20,10 +23,24 @@ export function Register() {
           </Link>
         </p>
       </header>
-      <form className='mt-[60px] flex flex-col gap-4'>
-        <Input name='name' placeholder='Nome' />
-        <Input type='email' name='email' placeholder='E-mail' />
-        <Input type='password' name='password' placeholder='Senha' />
+      <form onSubmit={handleSubmit} className='mt-[60px] flex flex-col gap-4'>
+        <Input
+          placeholder='Nome'
+          error={errors.name?.message}
+          {...register('name')}
+        />
+        <Input
+          type='email'
+          placeholder='E-mail'
+          error={errors.email?.message}
+          {...register('email')}
+        />
+        <Input
+          type='password'
+          placeholder='Senha'
+          error={errors.password?.message}
+          {...register('password')}
+        />
         <Button type='submit' className='mt-2'>
           Criar conta
         </Button>
