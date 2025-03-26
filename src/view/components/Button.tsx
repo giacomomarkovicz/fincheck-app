78bb6a87d383550d0250be5299473b5b67b21e05
Giácomo <giacomocamposv2@hotmail.com>
2025-03-26 12:13:17 -0300

    fixed spinner on load

--- src/view/components/Button.tsx
+++ src/view/components/Button.tsx
@@ -1,5 +1,6 @@
 import { ComponentProps } from 'react'
 import { cn } from '../../app/utils/cn'
+import { Spinner } from './Spinner'
 
 interface ButtonProps extends ComponentProps<'button'> {
   isLoading?: boolean
@@ -9,6 +10,7 @@ export function Button({
   className,
   isLoading,
   disabled,
+  children,
   ...props
 }: ButtonProps) {
   return (
@@ -16,9 +18,11 @@ export function Button({
       {...props}
       disabled={disabled || isLoading}
       className={cn(
-        'h-12 rounded-2xl bg-teal-900 px-6 font-medium text-white transition-all hover:bg-teal-800 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-400',
+        'flex h-12 items-center justify-center rounded-2xl bg-teal-900 px-6 font-medium text-white transition-all hover:bg-teal-800 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-400',
         className
       )}
-    />
+    >
+      {isLoading ? <Spinner className='h-6 w-6' /> : children}
+    </button>
   )
 }
