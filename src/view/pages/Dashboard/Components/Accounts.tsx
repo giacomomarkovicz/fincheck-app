234ce21fed37879c2745d60f3442886689159df7
Giácomo <giacomocamposv2@hotmail.com>
2025-03-28 15:32:24 -0300

    accounts and transactions components

---
+++ src/view/pages/Dashboard/Components/Accounts.tsx
@@ -0,0 +1,7 @@
+export function Accounts() {
+  return (
+    <div className='h-full w-full rounded-2xl bg-teal-900 px-4 py-8 md:p-10'>
+      Accounts
+    </div>
+  )
+}
--- src/view/pages/Dashboard/index.tsx
+++ src/view/pages/Dashboard/index.tsx
@@ -1,8 +1,23 @@
-import { useAuth } from "../../../app/hooks/useAuth"
+import { Logo } from '../../components/Logo'
+import { UserMenu } from '../../components/UserMenu'
+import { Accounts } from './Components/Accounts'
+import { Transactions } from './Components/Transactions'
 
 export function Dashboard() {
-
-  const { signout } = useAuth()
-
-  return <h1>Dashboard Page</h1>
+  return (
+    <div className='flex h-full w-full flex-col gap-4 p-4 md:p-8 md:pt-6'>
+      <header className='flex h-12 items-center justify-between'>
+        <Logo className='h-6 text-teal-900' />
+        <UserMenu />
+      </header>
+      <main className='flex flex-1 flex-col gap-4 md:flex-row'>
+        <div className='w-full md:w-1/2'>
+          <Accounts />
+        </div>
+        <div className='w-full md:w-1/2'>
+          <Transactions />
+        </div>
+      </main>
+    </div>
+  )
 }
