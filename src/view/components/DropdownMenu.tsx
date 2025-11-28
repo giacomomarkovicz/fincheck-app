9ece1c2479a22f99460d4b95111183c6213e7cae
Giácomo <giacomocamposv2@hotmail.com>
2025-11-28 17:07:45 -0300

    feat: added user menu dropdown

---
+++ src/view/components/DropdownMenu.tsx
@@ -0,0 +1,85 @@
+import * as RdxDropdownMenu from '@radix-ui/react-dropdown-menu'
+import { cn } from '../../app/utils/cn'
+
+interface DropdownMenuItemProps {
+  children?: React.ReactNode
+  className?: string
+  onSelect?: () => void
+}
+
+interface DropdownMenuContentProps {
+  children?: React.ReactNode
+  className?: string
+}
+
+interface DropdownMenuTriggerProps {
+  children?: React.ReactNode
+  className?: string
+}
+
+function DropdownMenuRoot({ children }: { children?: React.ReactNode }) {
+  return <RdxDropdownMenu.Root>{children}</RdxDropdownMenu.Root>
+}
+
+function DropdownMenuTrigger({
+  children,
+  className
+}: DropdownMenuTriggerProps) {
+  return (
+    <RdxDropdownMenu.Trigger
+      className={cn(
+        'outline-none focus:outline-none focus-visible:outline-none',
+        className
+      )}
+    >
+      {children}
+    </RdxDropdownMenu.Trigger>
+  )
+}
+
+function DropdownMenuContent({
+  children,
+  className
+}: DropdownMenuContentProps) {
+  return (
+    <RdxDropdownMenu.Portal>
+      <RdxDropdownMenu.Content
+        align='end'
+        sideOffset={4}
+        className={cn(
+          'z-50 space-y-2 rounded-2xl bg-white p-2 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.1)] will-change-[opacity,transform]',
+          'data-[side=bottom]:animate-slide-up-and-fade',
+          'data-[side=top]:animate-slide-down-and-fade',
+          className
+        )}
+      >
+        {children}
+      </RdxDropdownMenu.Content>
+    </RdxDropdownMenu.Portal>
+  )
+}
+
+function DropdownMenuItem({
+  children,
+  className,
+  onSelect
+}: DropdownMenuItemProps) {
+  return (
+    <RdxDropdownMenu.Item
+      onSelect={onSelect}
+      className={cn(
+        'flex min-h-[40px] cursor-pointer items-center rounded-2xl px-4 py-2 text-sm text-gray-800 transition-colors outline-none data-[highlighted]:bg-gray-50',
+        className
+      )}
+    >
+      {children}
+    </RdxDropdownMenu.Item>
+  )
+}
+
+export const DropdownMenu = {
+  Root: DropdownMenuRoot,
+  Trigger: DropdownMenuTrigger,
+  Content: DropdownMenuContent,
+  Item: DropdownMenuItem
+}
--- src/view/components/UserMenu.tsx
+++ src/view/components/UserMenu.tsx
@@ -1,9 +1,28 @@
+import { ExitIcon } from '@radix-ui/react-icons'
+import { DropdownMenu } from './DropdownMenu'
+import { useAuth } from '../../app/hooks/useAuth'
+
 export function UserMenu() {
+  const { signout } = useAuth()
+
   return (
-    <div className='bg-teal-0 flex h-12 w-12 items-center justify-center rounded-full border border-teal-100'>
-      <span className='text-sm font-medium tracking-[-0.5px] text-teal-900'>
-        GC
-      </span>
-    </div>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <div className='bg-teal-0 flex h-12 w-12 cursor-pointer items-center justify-center rounded-full border border-teal-100 hover:border-teal-900 focus:border-teal-900'>
+          <span className='text-sm font-medium tracking-[-0.5px] text-teal-900'>
+            GC
+          </span>
+        </div>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Content className='w-32'>
+        <DropdownMenu.Item
+          onSelect={signout}
+          className='flex items-center justify-between'
+        >
+          Sair <ExitIcon className='h-4 w-4' />
+        </DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
   )
 }
