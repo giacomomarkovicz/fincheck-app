0e74019044116ea3fc4e2e832bc64948391e0ff7
Giácomo <giacomocamposv2@hotmail.com>
2025-12-11 16:36:27 -0300

    wip: filters modal

---
+++ src/view/pages/Dashboard/Components/Transactions/FiltersModal.tsx
@@ -0,0 +1,19 @@
+import { Modal } from '../../../../components/Modal'
+
+interface FiltersModalProps {
+    open?: boolean
+    onClose?: () => void
+}
+
+export function FiltersModal({ open, onClose }: FiltersModalProps) {
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      title='Filtros'
+      rightAction={null}
+    >
+      Conteudo
+    </Modal>
+  )
+}
--- src/view/pages/Dashboard/Components/Transactions/useTransactionsController.ts
+++ src/view/pages/Dashboard/Components/Transactions/useTransactionsController.ts
@@ -1,7 +1,26 @@
+import { useState } from 'react'
 import { useDashboard } from '../DashboardContext/useDashboard'
 
 export function useTransactionsController() {
   const { areValuesVisible } = useDashboard()
 
-  return { areValuesVisible, isInitialLoading: false, isLoading: false, transactions: [{}] }
+  const [isFilterModalOpen, setIsFilterModalOpen] = useState<boolean>(false)
+
+  function handleOpenFilterModal() {
+    setIsFilterModalOpen(true)
+  }
+
+  function handleCloseFilterModal() {
+    setIsFilterModalOpen(false)
+  }
+
+  return {
+    areValuesVisible,
+    isInitialLoading: false,
+    isLoading: false,
+    isFilterModalOpen,
+    handleCloseFilterModal,
+    handleOpenFilterModal,
+    transactions: [{}]
+  }
 }
