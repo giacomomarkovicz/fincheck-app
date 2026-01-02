e347f5e6b9b1fbc285261cd773de3ebd249629a6
Giácomo <giacomocamposv2@hotmail.com>
2026-01-02 17:41:49 -0300

    feat: added  actions and state to filters modal

---
+++ src/view/pages/Dashboard/Components/Transactions/FiltersModal/index.tsx
@@ -0,0 +1,63 @@
+import { ChevronLeftIcon, ChevronRightIcon } from '@radix-ui/react-icons'
+import { Modal } from '../../../../../components/Modal'
+import { Button } from '../../../../../components/Button'
+import { useFiltersModal } from './useFiltersModal'
+import { cn } from '../../../../../../app/utils/cn'
+
+interface FiltersModalProps {
+  open?: boolean
+  onClose?: () => void
+}
+
+const mockedAccounts = [
+  { id: '1', name: 'Nubank' },
+  { id: '2', name: 'XP' },
+  { id: '3', name: 'Dinheiro' }
+]
+
+export function FiltersModal({ open, onClose }: FiltersModalProps) {
+  const { selectedBankAccountId, selectedYear, handleSelectBankAccount, handleChangeYear } =
+    useFiltersModal()
+
+  return (
+    <Modal open={open} onClose={onClose} title='Filtros' rightAction={null}>
+      <div>
+        <span className='text-lg font-bold tracking-[-1px] text-gray-800'>
+          Conta
+        </span>
+
+        <div className='mt-2 space-y-2'>
+          {mockedAccounts.map((account) => (
+            <button
+              key={account.id}
+              onClick={() => handleSelectBankAccount(account.id)}
+              className={cn('w-full rounded-2xl p-2 text-left text-gray-800 transition-colors hover:bg-gray-50',
+                account.id === selectedBankAccountId && '!bg-gray-200'
+              )}
+            >
+              {account.name}
+            </button>
+          ))}
+        </div>
+      </div>
+
+      <div className='mt-10 text-gray-800'>
+        <span className='text-lg font-bold tracking-[-1px]'>Ano</span>
+
+        <div className='mt-2 flex w-52 items-center justify-between'>
+          <button onClick={() => handleChangeYear(-1)} className='flex h-12 w-12 items-center justify-center'>
+            <ChevronLeftIcon className='h-6 w-6' />
+          </button>
+          <div className='flex-1 text-center'>
+            <span className='text-sm font-medium tracking-[-0.5px]'>{selectedYear}</span>
+          </div>
+          <button onClick={() => handleChangeYear(1)} className='flex h-12 w-12 items-center justify-center'>
+            <ChevronRightIcon className='h-6 w-6' />
+          </button>
+        </div>
+      </div>
+
+      <Button className='mt-10 w-full'>Aplicar Filtros</Button>
+    </Modal>
+  )
+}
